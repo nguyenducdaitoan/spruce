a20fe4878bf5712401a008b06814a22392716006
nguyenducdaitoan <nguyenducdaitoan@gmail.com>
2016-04-10 11:52:50 -0700

    update procfile

--- application/modules/admin/views/ajax_get_city.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($cities as $city){ ?>
-<option value="<?php echo $city->id; ?>"><?php echo $city->name; ?></option>
-<?php }?>
--- application/modules/admin/views/ajax_get_state.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($state as $stt){ ?>
-<option value="<?php echo $stt->id; ?>"><?php echo $stt->name; ?></option>
-<?php }?>
